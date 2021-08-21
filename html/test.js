a16dead733bf9cf75f474e5369d19aa8dcad443b
ryu <ddukes.tk@gmail.com>
2021-08-21 17:55:01 +0900

    added b02_html

---
+++ html/test.js
@@ -0,0 +1,34 @@
+$(function () {
+  $(document).on("change", ".tab__input", function () {
+    var description = "";
+    var breadcrumb = "";
+    var title = "";
+
+    if ($(this).val() === "store") {
+      description =
+        "イオンペット グルーミング予約サイトの「検索結果（リスト）」ページです。イオンペットは、動物と人間の幸せな共生社会の実現を目指します。";
+      breadcrumb = "検索結果（リスト）";
+      title =
+        "店舗検索＞検索結果（リスト）｜イオンペット グルーミング予約サイト";
+      replace(description, breadcrumb, title);
+
+      $(".tab__map").fadeOut();
+      $(".tab__store").fadeIn();
+    } else if ($(this).val() === "map") {
+      description =
+        "イオンペット グルーミング予約サイトの「検索結果（地図）」ページです。イオンペットは、動物と人間の幸せな共生社会の実現を目指します。";
+      breadcrumb = "検索結果（地図）";
+      title = "店舗検索＞検索結果（地図）｜イオンペット グルーミング予約サイト";
+      replace(description, breadcrumb, title);
+
+      $(".tab__store").fadeOut();
+      $(".tab__map").fadeIn();
+    }
+
+    function replace(description, breadcrumb, title) {
+      $("meta[name='description']").attr("content", description);
+      $("title").html(title);
+      $(".brandcrumb__item-link").last().text(breadcrumb);
+    }
+  });
+});
