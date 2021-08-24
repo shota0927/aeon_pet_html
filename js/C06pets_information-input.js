bac42bf433534a1c52d8434289e2777fe5457564
ryu <ddukes.tk@gmail.com>
2021-08-24 18:43:42 +0900

    edited

--- js/C06pets_information-input.js
+++ js/C06pets_information-input.js
@@ -1,12 +1,32 @@
 $(function () {
+  var preModalPet="";
+
+
+  $(".modal-container__content").on("click", function () {
+    event.stopPropagation();
+  });
+
   $(document).on("click", ".modal-open", function () {
     $(".modal-container").fadeIn();
-    console.log("てす");
+    preModalPet="#"+$("input[name='radio__dog-kind']:checked")[0].id;
     return false;
   });
   $(".modal-close").on("click", function () {
+    $("input[name='radio__dog-kind']:checked").prop('checked', false);
+    $(preModalPet).prop('checked', true);
     $(".modal-container").fadeOut();
-    console.log("てす");
+    return false;
+  });
+
+  $(".cancel-kind").on("click", function () {
+    $(preModalPet).prop('checked', true);
+    $(".modal-container").fadeOut();
+    return false;
+  });  
+
+  $(".decide-kind").on("click", function () {
+    $("input[name='radio__dog-kind']:checked").prop('checked', true);
+    $(".modal-container").fadeOut();
     return false;
   });
 });
